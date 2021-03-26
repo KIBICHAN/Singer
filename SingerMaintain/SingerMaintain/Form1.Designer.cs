83d6e9f8b98f12a55be95d564d1b6d2e9fc10be5
KIBICHAN <52041113+KIBICHAN@users.noreply.github.com>
2021-03-26 20:14:21 +0700

    Sonar

---
+++ SingerMaintain/SingerMaintain/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace SingerMaintain
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSingerList = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSingerList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSingerList
+            // 
+            this.dgvSingerList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSingerList.Location = new System.Drawing.Point(8, 253);
+            this.dgvSingerList.Name = "dgvSingerList";
+            this.dgvSingerList.RowHeadersWidth = 51;
+            this.dgvSingerList.RowTemplate.Height = 29;
+            this.dgvSingerList.Size = new System.Drawing.Size(780, 185);
+            this.dgvSingerList.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(664, 44);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(664, 134);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 41);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(664, 96);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(94, 29);
+            this.btnFind.TabIndex = 3;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(664, 205);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(94, 29);
+            this.btnView.TabIndex = 4;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(88, 53);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(538, 27);
+            this.txtID.TabIndex = 9;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(88, 105);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(538, 27);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(88, 158);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(538, 27);
+            this.txtAge.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(88, 207);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(543, 27);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvSingerList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSingerList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSingerList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnView;
+    }
+}
+
---
+++ SingerMaintain/SingerMaintain/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Castle.Components.DictionaryAdapter;
+using SingerLibrary;
+
+namespace SingerMaintain
+{
+    public partial class Form1 : Form
+    {
+        SingerData sd = new SingerData();
+        DataTable dtSinger;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnView_Click_1(object sender, EventArgs e)
+        {
+            getAllSinger();
+        }
+
+        private void getAllSinger()
+        {
+            dtSinger = sd.getSinger();
+
+            txtID.DataBindings.Clear();
+            txtName.DataBindings.Clear();
+            txtAge.DataBindings.Clear();
+            txtEmail.DataBindings.Clear();
+
+            txtID.DataBindings.Add("Text", dtSinger, "SingerID");
+            txtName.DataBindings.Add("Text", dtSinger, "SingerName");
+            txtAge.DataBindings.Add("Text", dtSinger, "SingerAge");
+            txtEmail.DataBindings.Add("Text", dtSinger, "SingerEmail");
+
+            dgvSingerList.DataSource = dtSinger;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(txtID.Text);
+            string Name = txtName.Text;
+            int Age = int.Parse(txtAge.Text);
+            string Email = txtEmail.Text;
+            Singer singer = new Singer
+            {
+                SingerID = ID,
+                SingerName = Name,
+                SingerAge = Age,
+                SingerEmail = Email
+            };
+            bool check = sd.addSinger(singer);
+            string message = (check == true ? "successfull" : "fail");
+            MessageBox.Show("Add " + message);
+            getAllSinger();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(txtID.Text);
+            Singer singer = sd.findSinger(ID);
+            string message = (singer != null ? "succesfull" : "fail");
+            MessageBox.Show("Find " + message);
+            BindingList<Singer> bl = new BindingList<Singer>();
+            bl.Add(singer);
+            dgvSingerList.DataSource = bl;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(txtID.Text);
+            bool check = sd.deleteSinger(ID);
+            string message = (check == true ? "succesfull" : "fail");
+            MessageBox.Show("Delete " + message);
+            getAllSinger();
+        }
+    }
+}
